708f4e0396006851c2f0ee7e47c48f256a403ebe
LENOVO <khanh68890912@gmail.com>
2022-11-02 18:54:32 +0700

    test

--- app/src/main/java/fpoly/edu/poly/luckyshit/MainActivity.java
+++ app/src/main/java/fpoly/edu/poly/luckyshit/MainActivity.java
@@ -16,7 +16,7 @@
     import java.util.ArrayList;
     import java.util.List;
     import java.util.Random;
-
+///tesst
 
     public class MainActivity extends AppCompatActivity {
         private LuckyWheel lw;
